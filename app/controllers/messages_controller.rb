1b117d13e8d34c1885ddb7fb07da8d9bb4b063de
tsubasaaaa! <tsubasa19960515@gmail.com>
2019-05-19 18:52:15 +0900

    routes view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,7 @@ class MessagesController < ApplicationController
   def index
   end
   
+  def create
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
