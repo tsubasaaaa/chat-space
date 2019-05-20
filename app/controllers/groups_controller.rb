2e1b067703e9ea4775cd12b18c40fca8e5981cc1
tsubasaaaa! <tsubasa19960515@gmail.com>
2019-05-20 13:05:39 +0900

    group-creation-function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,37 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+  def index
+    
+  end
+
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def edit
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループ編集しました'
+    else
+      render :edit
+    end
   end
   
+  private
+  def group_params
+    params.permit(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
 end
