df8ebab16cdba3d3e9dec57dc50f4d9ee1a4a728
tsubasaaaa! <tsubasa19960515@gmail.com>
2019-05-26 16:54:56 +0900

    fix automatic-updating2

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,7 +2,6 @@ class Api::MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
     @messages = @group.messages.where("id > ?", params[:last_message_id])
-    binding.pry
     respond_to do |format|
       format.html
       format.json 
